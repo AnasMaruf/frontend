fae48c33a151b53194acbe827baaea6636d569ae
AnasMaruf <anasmaruf1101@gmail.com>
2024-02-05 19:30:11 +0700

    Update

--- src/api/AuthApi.jsx
+++ src/api/AuthApi.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const register = async (data) => {
-  const response = await axios.post("/api/users", data);
+  const response = await axios.post("http://localhost:3000/api/users", data);
   return response;
 };
 
 const login = async () => {
-  const response = await axios.post("/api/users/login");
+  const response = await axios.post("http://localhost:3000/api/users/login");
   return response;
 };
 export { register, login };
